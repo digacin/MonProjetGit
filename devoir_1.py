fdde2234adbe3df0b39441730429290a2139b5fb
Nicxon Digacin <nicxon.digacin.1@ulaval.ca>
2025-09-12 00:02:16 -0400

    Ajout de parties additionnelles

--- devoir_1.py
+++ devoir_1.py
@@ -8,4 +8,49 @@
 a=8
 b=-a
 c=a + b
-print("Le résultat est:",c)
+print("REPONSE-1: Le résultat est:",c)
+
+#Question 2: Donnez les valeurs des variables a, b et c après l'exécution du pseudo-code suivant:
+#a <- 5
+#cb <- 10 + 2
+# c <- a + b - 3
+# b <- a
+
+#Reponse a la question 1:
+a=5
+b=10 + 2
+c= a + b - 3
+b=a
+
+print("REPONSE-2: Le resultat est:",a,b,c)
+
+#Question #3: Donnez les valeurs des variables a, b et c après l'exécution du pseudo-code suivant:
+
+a= 1
+b= a
+a= a + a
+a= a * a * a
+c= a + b
+print("REPONSE-3: Le resultat est:",a,b,c)
+
+
+#Soit la fonction mathématique y = 2x+1, où x et y sont réels. Remplissez ce programme trouvant le y associé à un x en remplaçant la ligne # par votre code:
+x = int(input("x: "))
+
+y = 2 * x + 1
+
+print("y =", y)
+
+y= float (input("y:"))
+
+x= (1-y)/2
+
+print ("x = ", x)
+
+nombre_1 =int(input("Un premier nombre:"))
+
+nombre_2 = int(input("Un deuxième nombre:"))
+
+somme =nombre_1+nombre_2
+
+print("La somme est:", somme)
