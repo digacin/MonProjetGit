ed3e9469340dd53b5dc9d6a91f17554961b3831b
Nicxon Digacin <nicxon.digacin.1@ulaval.ca>
2025-09-11 21:54:59 -0400

    Devoir 1 avec reponses

---
+++ devoir_1.py
@@ -0,0 +1,11 @@
+#Question 1: Donnez les valeurs des variables a, b et c après l'exécution du pseudo-code suivant:
+
+# a <- 8
+#b <- -a
+#c <- a + b //
+
+#Reponse a la question 1:
+a=8
+b=-a
+c=a + b
+print("Le résultat est:",c)
